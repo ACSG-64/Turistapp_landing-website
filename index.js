978722e441ebe53c91067455dede6fc5625bfa21
Andrés Sarmiento <acamilo707@gmail.com>
2021-08-08 12:43:02 -0500

    Responsive design added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const phone = document.getElementById("features_phone_img")
 
 function changePhoneImg(feature) {
-    if (feature === "list")
-        phone.style = "background-image: url('media/app/list_screen.png');"
-    else if (feature === "map")
-        phone.style = "background-image: url('media/app/map_screen.png');"
-    else if (feature === "info")
-        phone.style = "background-image: url('media/app/details_screen.png');"
+    if(window.innerWidth > 760) {
+        if (feature === "list")
+            phone.style = "background-image: url('media/app/desktop/list_screen.png');"
+        else if (feature === "map")
+            phone.style = "background-image: url('media/app/desktop/map_screen.png');"
+        else if (feature === "info")
+            phone.style = "background-image: url('media/app/desktop/details_screen.png');"
+    }
 }
 
 
